c7aa2fee95d19b1753c44bffa29c972ed86a7317
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-02-10 09:17:01 +0200

    ddd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+
 import Welcome from "./components/Welcome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
@@ -6,46 +8,68 @@ import SignIn from "./components/signIn";
 import Landing from "./components/landing";
 import Hotel from "./components/hotel";
 import AddRooms from "./components/AddRooms";
-import AddHotels from  "./components/AddHotels";
+import AddHotels from "./components/AddHotels";
 import Bookings from "./components/bookings";
-import Admin from './components/admin';
-import Guests from './components/guests';
-import Notifications from './components/notifications';
-import Rooms from './components/rooms';
+import Admin from "./components/admin";
+import Guests from "./components/guests";
+import Notifications from "./components/notifications";
+import Rooms from "./components/rooms";
 import About from "./components/about";
 import AdminView from "./components/adminView";
 import RoomView from "./components/roomView";
 import Signup from "./components/Signup";
 import Home from "./components/home";
+import { useNavigate } from "react-router-dom";
+
+import { auth } from "./firebase";
+
 function App() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        navigate("/landing");
+      }
+    });
+  }, []);
+  console.log(user);
   return (
+    // <Router>
     <div className="app">
-      <Router>
-      <div style={{display:'flex'}}>
-        <Home />
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/homepage" element={<Homepage />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/hotel" element={<Hotel />} />
-          <Route path="/AddRooms" element={<AddRooms />} />
-          <Route path="/AddHotels" element={<AddHotels />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/bookings" element={<Bookings />} />
-
-        <Route path="/guests" element={<Guests />} />
-        <Route path="/admin" element={<Admin/>} />
-        <Route path="/notifications" element={<Notifications/>} /> 
-        <Route path="/rooms" element={<Rooms/>} /> 
-        <Route path="/adminView" element={<AdminView/>} /> 
-        <Route path="/roomView" element={<RoomView/>} /> 
-        <Route path="/Signup" element={<Signup/>}/>
-        </Routes>
-        </div>
-
-      </Router>
+      <div style={{ display: "flex" }}>
+        {user ? (
+          <>
+            <Home />
+            <Routes>
+              <Route path="/landing" element={<Landing />} />
+                  <Route path="/hotel" element={<Hotel />} />
+                  <Route path="/AddRooms" element={<AddRooms />} />
+                  <Route path="/AddHotels" element={<AddHotels />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/bookings" element={<Bookings />} />
+                  <Route path="/guests" element={<Guests />} />
+                  <Route path="/admin" element={<Admin />} />
+                  <Route path="/notifications" element={<Notifications />} />
+                  <Route path="/rooms" element={<Rooms />} />
+                  <Route path="/adminView" element={<AdminView />} />
+                  <Route path="/roomView" element={<RoomView />} />
+            </Routes>
+          </>
+        ) : (
+          <>
+            <Routes>
+              <Route path="/" element={<SignIn setUser={setUser} />} />
+              <Route path="/Signup" element={<Signup />} />
+            </Routes>
+          </>
+        )}
+      </div>
     </div>
+    // </Router>
   );
 }
 
--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -1,103 +1,130 @@
-import React, {useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 //import { signInWithEmailAndPassword } from "firebase/auth";
 import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { auth } from "../firebase.js";
 import { useNavigate } from "react-router-dom";
-import pin3 from '../images/pin3.jpg';
+import pin3 from "../images/pin3.jpg";
 
 import "../App.css";
 
-const SignIn = () => {
+const SignIn = ({setUser}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+
   const navigate = useNavigate();
 
-  useEffect(() =>{
-    auth.onAuthStateChanged((user) =>{
-      if(user){
-        navigate('/landing')
-      }
-    })
-  })
-  
-  const handleEmailChange = (e) =>{
-    setEmail(e.target.value);
-  }
+  // useEffect(() => {
+  //   auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       setUser(user)
+  //       navigate("/landing");
+  //     }
+  //   });
+  // });
 
-  const handlePasswordChange = (e) =>{
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-  }
+  };
 
   const handleSignIn = () => {
-    signInWithEmailAndPassword(auth, email, password).then(() => {
-      navigate('/landing')
-    }).catch((err) => 
-    alert(err.message)
-    );
-  }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((user) => {
+        console.log(user)
+        navigate("/landing"); 
+      })
+      .catch((err) => alert(err.message));
+  };
 
-    return(
+  return (
     <>
+      <div>
+        <nav className="navigation">
+          <div className="ImageHeader">
+            <img
+              src={pin3}
+              alt="this is car image"
+              style={{ marginLeft: 80, height: "39%", marginTop: 27 }}
+            />
+            <h1
+              style={{
+                color: "#fff",
+                alignContent: "center",
+                marginLeft: 240,
+                marginTop: 90,
+              }}
+            >
+              {" "}
+              Hotel Admin System{" "}
+            </h1>
+          </div>
+        </nav>
 
-<div>
-    <nav className="navigation">
-      
-    <div className="ImageHeader" >
-   <img src={pin3} alt="this is car image" style={{marginLeft:80, height:'39%', marginTop:27}} /> 
-    <h1 style={{color:'#fff',alignContent:'center', marginLeft:240, marginTop:90}}>  Hotel Admin System </h1>
-    </div>
-    </nav>
+        <div className="fluid-container">
+          <h3 style={{ color: "#FF8038", textAlign: "center" }}>Sign In</h3>
+          <div className="col-md-8 mx-auto col-10 card shadow-lg border-0 p-4">
+            <div className="login-register">
+              <div class="mb-3 col-xs-4">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Email Address
+                </label>
 
-    <div className="fluid-container">
-            <h3 style={{color:'#FF8038',textAlign:'center'}}>Sign In</h3>
-  <div className='col-md-8 mx-auto col-10 card shadow-lg border-0 p-4'>
-  <div className="login-register">
-  <div class="mb-3 col-xs-4">
-  <label for="exampleFormControlInput1" class="form-label">Email Address</label>
-  
-  <input type="email" class="form-control w-75"
-   id="exampleFormControlInput1"
-  placeholder="Makgathokln@gmail.com"
-  onChange={handleEmailChange} value={email}
-  />
-</div>
-<div class="mb-3 col-xs-4">
-  <label for="exampleFormControlTextarea1" class="form-label">Password</label>
-  <input type="password"
-   class="form-control w-75" id="exampleFormControlInput1"
-    placeholder="********"
-    onChange={handlePasswordChange}
-      value={password}
-    />
-</div>
+                <input
+                  type="email"
+                  class="form-control w-75"
+                  id="exampleFormControlInput1"
+                  placeholder="Makgathokln@gmail.com"
+                  onChange={handleEmailChange}
+                  value={email}
+                />
+              </div>
+              <div class="mb-3 col-xs-4">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="form-control w-75"
+                  id="exampleFormControlInput1"
+                  placeholder="********"
+                  onChange={handlePasswordChange}
+                  value={password}
+                />
+              </div>
 
-<div class="mb-3 col-xs-4">
-<a href="#" class="link-secondary">Forgot Your Password?</a>
-  </div>
+              <div class="mb-3 col-xs-4">
+                <a href="#" class="link-secondary">
+                  Forgot Your Password?
+                </a>
+              </div>
 
-  <div class="row">
-  <button type="button" onClick={handleSignIn} class="btn btn-light  w-25">Sign In</button>
-  <button type="button"  class="btn btn-light  w-25">Sign Up</button>
-
-  </div>
-  </div>
- 
-</div>
-
-</div>
-
-</div>
+              <div class="row">
+                <button
+                  type="button"
+                  onClick={handleSignIn}
+                  class="btn btn-light  w-25"
+                >
+                  Sign In
+                </button>
+                <button type="button" class="btn btn-light  w-25">
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-    )
-    
-}
+  );
+};
 
-
-    export default SignIn
+export default SignIn;
